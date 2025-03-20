5ac1f6e65b8f896f3e490c624f424ea781908346
origin <kruglov56832@mail.ru>
2025-03-20 18:06:15 +0900

    Wishlist_2

---
+++ src/main/java/com/wishlist/wishlist2/Wishlist2Application.java
@@ -0,0 +1,13 @@
+package com.wishlist.wishlist2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wishlist2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wishlist2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/wishlist/wishlist2/Wishlist2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wishlist.wishlist2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wishlist2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
